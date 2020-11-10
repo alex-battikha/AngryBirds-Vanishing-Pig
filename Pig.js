9c806a8d2c167073a63bfbbc285ea369a4dc50eb
alex-battikha <56514843+alex-battikha@users.noreply.github.com>
2020-11-09 18:58:28 -0800

    vanishing pigs

--- Pig.js
+++ Pig.js
@@ -2,6 +2,23 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
+    this.visibility = 255;
+  }
+
+  display(){
+    var pos = this.body.position;
+
+    if(this.body.speed<3){
+      super.display();
+    }
+    else {
+      push();
+      World.remove(world, this.body);
+      this.visibility-=5;
+      tint(255, this.visibility);
+      image(this.image, pos.x, pos.y);
+      pop();
+    }
   }
 
 };
